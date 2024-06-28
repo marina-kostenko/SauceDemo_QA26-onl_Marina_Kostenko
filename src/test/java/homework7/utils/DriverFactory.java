b4bb4cf73af80c97e2198d3cb860a019c20632a0
marina-kostenko <89marisha@gmail.com>
2024-06-28 22:27:32 +0300

    Log4j2, Lombok added

--- src/test/java/homework7/utils/DriverFactory.java
+++ src/test/java/homework7/utils/DriverFactory.java
@@ -1,15 +1,19 @@
 package homework7.utils;
 
+
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
+@Log4j2
 public class DriverFactory {
     public static WebDriver getDriver() throws Exception
     {
         String browserName = System.getProperty("browser", "chrome");
+        log.debug("Requested browser: {}", browserName);
         WebDriver driver;
         switch (browserName.toLowerCase()) {
             case "chrome":
